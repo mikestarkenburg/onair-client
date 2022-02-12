48099aa0ca1dcc473ff61362feb80fddf9508c3c
Michael Starkenburg <mike@starkenburg.com>
2022-02-11 17:45:23 -0800

    add webhooks

--- onair-client.ps1
+++ onair-client.ps1
@@ -2,13 +2,19 @@
 #
 # todo:  light webhooks, logging
 
-$ErrorActionPreference= 'silentlycontinue'
+# $ErrorActionPreference= 'silentlycontinue'
 if (Get-Process Zoom) 
 {
    # Zoom is active and there are ZERO UDP packets
    If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) {Write-Host "zoom is open but no meeting"}
+   Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=OFF
 
    # Zoom is active and there are UDP packets
    If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) {Write-Host "Mike is in a meeting"}
+   Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=BLUE
 }
-else {Write-host "there is no zoom process"}
+else 
+{
+   Write-host "there is no zoom process"
+   Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=OFF
+}
