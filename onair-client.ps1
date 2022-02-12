76e648cfea4417c9bc52574372285e91fd239336
Michael Starkenburg <mike@starkenburg.com>
2022-02-11 17:39:43 -0800

    initial commit

---
+++ onair-client.ps1
@@ -0,0 +1,13 @@
+# Stark's Onair Windows Powershell Client Script
+#
+
+$ErrorActionPreference= 'silentlycontinue'
+if (Get-Process Zoom) 
+{
+   # Zoom is active and there are ZERO UDP packets
+   If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) {Write-Host "zoom is open but no meeting"}
+
+   # Zoom is active and there are UDP packets
+   If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) {Write-Host "Mike is in a meeting"}
+}
+else {Write-host "there is no zoom process"}
