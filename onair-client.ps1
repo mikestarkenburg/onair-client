84215407f54e53d0496ad083bad827a62ad77f33
Michael Starkenburg <mike@starkenburg.com>
2022-02-11 18:15:15 -0800

    variable for url

--- onair-client.ps1
+++ onair-client.ps1
@@ -1,27 +1,30 @@
 # Stark's Onair Windows Powershell Client Script
 #
-# todo:  variable webhooks, logging
+# todo:  logging
+
 
 $global:ErrorActionPreference= 'SilentlyContinue'
 $global:ProgressPreference = 'SilentlyContinue' 
+$url = "onair.starkenburg.com"
+
 if (Get-Process Zoom) 
 {
    # Zoom is active and there are ZERO UDP packets
    If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) 
    {
       Write-Host "zoom is open but no meeting"
-      Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=OFF | Out-Null
+      Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
    }
 
    # Zoom is active and there are UDP packets
    If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) 
    {
       Write-Host "Mike is in a meeting"
-      Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=BLUE | Out-Null
+      Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=BLUE | Out-Null
    }
 }
 else 
 {
    Write-host "there is no zoom process"
-   Invoke-WebRequest -UseBasicParsing -Uri 192.168.5.110/?LED=OFF | Out-Null
+   Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
 }
