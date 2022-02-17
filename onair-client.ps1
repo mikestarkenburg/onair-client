5f6f08aedd2287fee1962bf72af0b25cb400eecd
Michael Starkenburg <mike@starkenburg.com>
2022-02-16 17:45:27 -0800

    run every x seconds, variable user, variable URL

--- onair-client.ps1
+++ onair-client.ps1
@@ -1,30 +1,38 @@
 # Stark's Onair Windows Powershell Client Script
 #
-# todo:  logging
+# todo:  only ping on change of state, tray icon, start on boot, logging
 
 
 $global:ErrorActionPreference= 'SilentlyContinue'
 $global:ProgressPreference = 'SilentlyContinue' 
 $url = "onair.starkenburg.com"
+$user = "Mike"
+$seconds = 5
+Do {
 
-if (Get-Process Zoom) 
-{
-   # Zoom is active and there are ZERO UDP packets
-   If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) 
+   if (Get-Process Zoom) 
    {
-      Write-Host "zoom is open but no meeting"
+      # Zoom is active and there are ZERO UDP packets
+      If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) 
+      {
+         Write-Host "zoom is open but no meeting"
+         Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
+      }
+
+   # Zoom is active and there are UDP packets
+      If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) 
+      {
+         Write-Host $user + " is in a meeting"
+         Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=BLUE | Out-Null
+      }
+   } 
+   else 
+   {
+      Write-host "there is no zoom process"
       Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
    }
 
-   # Zoom is active and there are UDP packets
-   If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) 
-   {
-      Write-Host "Mike is in a meeting"
-      Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=BLUE | Out-Null
-   }
+   Start-Sleep $seconds
+   
 }
-else 
-{
-   Write-host "there is no zoom process"
-   Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
-}
+while ($true)
