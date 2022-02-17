7e535bc8be2541654bbb0ed173b2263876234496
Michael Starkenburg <mike@starkenburg.com>
2022-02-16 18:59:40 -0800

    only ping the device on a change of state

--- onair-client.ps1
+++ onair-client.ps1
@@ -1,6 +1,6 @@
 # Stark's Onair Windows Powershell Client Script
 #
-# todo:  only ping on change of state, tray icon, start on boot, logging
+# todo:  tray icon, start on boot, logging
 
 
 $global:ErrorActionPreference= 'SilentlyContinue'
@@ -8,32 +8,45 @@ $global:ProgressPreference = 'SilentlyContinue'
 $url = "onair.starkenburg.com"
 $user = "Mike"
 $seconds = 2
+$laststate = "openIdle"
 
-
-Do {
-   if (Get-Process Zoom) 
+Do 
+{
+   If (Get-Process Zoom) 
    {
       # Zoom is active and there are ZERO UDP packets
       If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -lt 1) 
       {
          Write-Host "zoom is open but no meeting"
-         Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
+         If ($laststate -ne "openIdle")
+         {
+            Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
+            Write-Host "Setting LED OFF"
+            $laststate = "openIdle"
+         }
       }
-
       # Zoom is active and there are UDP packets
       If ((Get-NetUDPEndpoint -OwningProcess (Get-Process Zoom).Id -EA 0|Measure-Object).count -gt 0) 
       {
-         Write-Host $user + " is in a meeting"
-         Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=BLUE | Out-Null
+         Write-Host $user "is in a meeting"
+         If ($laststate -ne "inMeeting")
+         {
+            Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=BLUE | Out-Null
+            Write-Host "Setting LED BLUE"
+            $laststate = "inMeeting"
+         }
       }
    } 
    else 
    {
       Write-host "there is no zoom process"
-      Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
+      If ($laststate -ne "noProcess")
+      {
+         Invoke-WebRequest -UseBasicParsing -Uri $url/?LED=OFF | Out-Null
+         Write-Host "Setting LED OFF"
+         $laststate = "noProcess"
+      }
    }
-
-   Start-Sleep $seconds
-   
+   Start-Sleep $seconds  
 }
 while ($true)
